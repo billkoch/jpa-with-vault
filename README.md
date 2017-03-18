5ee7c938bd73e6cf71c8050eca4c594a074746a7
William Koch <koch.149@osu.edu>
2017-03-18 00:05:37 -0400

    vault interations work; subsequent bootRuns don't

--- README.md
+++ README.md
@@ -17,35 +17,3 @@ $ docker-compose exec vault /bin/sh /tmp/setup.sh
 ```bash
 $ ./gradlew bootRun
 ```
-
-
-* Configure Vault CLI
-```bash
-$ export VAULT_TOKEN='root-vault-token' && \
-  export VAULT_ADDR='http://0.0.0.0:8200'
-```
-
-* Mount the PostgreSQL Secret Backend
-```bash
-$ vault mount postgresql
-```
-
-* Give Vault the connection information for PostgreSQL
-```bash
-$ vault write postgresql/config/connection \
-  connection_url="postgresql://root:postgresql-root-password@postgresql:5432/postgres?sslmode=disable"
-```
-
-* Establish the lease timeframe for the PostgreSQL `ALL` permission
-```bash
-$ vault write postgresql/config/lease \
-  lease=1m \
-  lease_max=5m
-```
-
-* Configure the SQL commands to provision a new account and revoke an account
-```bash
-$ vault write postgresql/roles/all \
-  sql="CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}';GRANT ALL ON *.* TO '{{name}}'@'%';" \
-  revocation_sql="DROP USER '{{name}}'@'%';"
-```
--- src/main/java/io/billkoch/examples/Application.java
+++ src/main/java/io/billkoch/examples/Application.java
@@ -15,13 +15,37 @@
  */
 package io.billkoch.examples;
 
+import io.billkoch.examples.domain.Person;
+import io.billkoch.examples.repositories.PersonRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+  @Autowired
+  private PersonRepository personRepository;
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    while(true) {
+      log.debug("Persisting a new person");
+      personRepository.save(new Person(RandomStringUtils.randomAlphabetic(5), RandomStringUtils.randomAlphabetic(8)));
+      log.debug("Now there are {} persisted people", personRepository.count());
+      long sleepTime = RandomUtils.nextLong(1000L, 6000L);
+      log.debug("Sleeping for {} milliseconds", sleepTime);
+      Thread.sleep(sleepTime);
+    }
   }
 }
